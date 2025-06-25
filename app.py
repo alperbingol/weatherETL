947bc3c3424de40f836cbdf55c8447d176feff8a
alperbingol <alperbingol@sabanciuniv.edu>
2025-06-25 14:55:46 +0200

    Feat changed the logic of timezone. It automaticallys fettches the timezone from the request and uses it for the final result. Add postCreateCommand to install dependencies automatically in Dev Container

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 import requests
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 from dateutil import parser
+from zoneinfo import ZoneInfo
 
+
+
+print("datetime.now():", datetime.now())
+print("datetime.now(timezone.utc):", datetime.now(timezone.utc))
 
 
 LATITUDE = 52.52
@@ -11,24 +16,30 @@ def get_weather():
     url = (
         f"https://api.open-meteo.com/v1/forecast"
         f"?latitude={LATITUDE}&longitude={LONGITUDE}"
-        f"&hourly=temperature_2m,relative_humidity_2m&timezone=UTC"
+        f"&hourly=temperature_2m,relative_humidity_2m&timezone=auto"
     )
 
     response = requests.get(url)
     response.raise_for_status()
     data = response.json()
 
-    # UTC-aware current hour
-    current_utc = datetime.now(timezone.utc).replace(minute=0, second=0, microsecond=0)
-    
-    times = [parser.isoparse(t).replace(tzinfo=timezone.utc) for t in data["hourly"]["time"]]
+    # Use the timezone returned by the API
+    api_timezone = data["timezone"]
+    current_local = datetime.now(ZoneInfo(api_timezone)).replace(minute=0, second=0, microsecond=0)
+    print(f"Current time in {api_timezone}: {current_local}")
+
+
+    # API already returns local time due to timezone=auto
+    #times = [parser.isoparse(t) for t in data["hourly"]["time"]]
+    times = [parser.isoparse(t).replace(tzinfo=ZoneInfo(api_timezone)) for t in data["hourly"]["time"]]
+
+
+    # Find closest time
+    closest_index = min(range(len(times)), key=lambda i: abs(times[i] - current_local))
 
     # Convert all returned times to datetime
     temperatures = data["hourly"]["temperature_2m"]
     humidities = data["hourly"]["relative_humidity_2m"]
-
-    # Find closest timestamp
-    closest_index = min(range(len(times)), key=lambda i: abs(times[i] - current_utc))
 
     return {
         "timestamp": times[closest_index].isoformat(),
